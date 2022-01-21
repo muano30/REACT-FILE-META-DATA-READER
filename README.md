76cb600c3c5e263f35e3b9e65fd532d881bf8462
muano30 <muanonevhufumba@gmail.com>
2022-01-21 12:56:11 +0200

    meta-data reader project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # REACT-FILE-META-DATA-READER
 
-When a user uppload an imagine they must get the information of the image they uploaded
+When a user uppload an imagine they must get the information of the image they uploaded , which is file name, file type, file size, and file as blob.
+
+Blob =  is section of memories set aside that contains data.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+
+function MetadataReader() {
+
+  const [image, setImage] = useState('');
+  const [imageInfo, setImageInfo] = useState([]);
+  const [blob, setBlob] = useState()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setImageInfo([...imageInfo, image])
+  }
+
+  const showBlob = (e,type) => {
+    const reader = new FileReader();
+    var blob = new Blob([reader.readAsText(e.target.files[0])], { type});
+    setBlob(JSON.stringify(blob))
+    
+  }
+  
+  const fileupload = (event) => {
+    if (event.target.files[0]) {
+      setImage(event.target.files[0]);
+      // console.log(event.target.files[0]);
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+
+        <h1>FILE META DATA READER</h1>
+        <input
+
+          type='file'
+          onChange={fileupload}
+        />
+
+        <button type="submit">Get File Data</button>
+
+      </form>
+      <ul>
+        {imageInfo.map((item, index) => {
+          return (
+            <p key={index}  onClick={(e) => showBlob(item.type)}>
+            <li>Name:  {item.name}</li>
+            <li>Size:  {item.size}</li>
+            <li>Type:  {item.type}</li>
+            <div>
+              {blob}
+            </div>
+          </p>
+
+          )
+        })
+      }
+      </ul>
     </div>
+
   );
 }
 
-export default App;
+export default MetadataReader;
